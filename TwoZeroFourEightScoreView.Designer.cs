6b8c0e266aa98f480c3190284a1a5367dc31298a
iamzack13110 <nutthakornzack1@gmail.com>
2015-10-23 12:46:52 +0700

    add homework

---
+++ TwoZeroFourEightScoreView.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace twozerofoureight
+{
+    partial class TwoZeroFourEightScoreView
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblScore = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblScore
+            // 
+            this.lblScore.AutoSize = true;
+            this.lblScore.Location = new System.Drawing.Point(53, 111);
+            this.lblScore.Name = "lblScore";
+            this.lblScore.Size = new System.Drawing.Size(46, 17);
+            this.lblScore.TabIndex = 0;
+            this.lblScore.Text = "label1";
+            // 
+            // TwoZeroFourEightScoreView
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(282, 253);
+            this.Controls.Add(this.lblScore);
+            this.Name = "TwoZeroFourEightScoreView";
+            this.Text = "TwoZeroFourEightScoreView";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.Label lblScore;
+    }
+}
---
+++ TwoZeroFourEightScoreView.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace twozerofoureight
+{
+    public partial class TwoZeroFourEightScoreView : Form
+    {
+        public TwoZeroFourEightScoreView()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
